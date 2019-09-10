ca7697bc37df9a66787636a3eddd6af323d4a693
SXILENT E <admin@elijahbrown.info>
2019-09-10 19:58:17 -0400

    test
    Signed-off-by: SXILENT E <admin@elijahbrown.info>

--- index.js
+++ index.js
@@ -1,42 +1,68 @@
-const webcamElement = document.getElementById('webcam');
+const webcamElement = document.getElementById("webcam");
 const classifier = knnClassifier.create();
 
 async function setupWebcam() {
   return new Promise((resolve, reject) => {
     const navigatorAny = navigator;
-    navigator.getUserMedia = navigator.getUserMedia ||
-      navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+    navigator.getUserMedia =
+      navigator.getUserMedia ||
+      navigatorAny.webkitGetUserMedia ||
+      navigatorAny.mozGetUserMedia ||
       navigatorAny.msGetUserMedia;
     if (navigator.getUserMedia) {
-      navigator.getUserMedia({
+      navigator.getUserMedia(
+        {
           video: true
         },
         stream => {
           webcamElement.srcObject = stream;
-          webcamElement.addEventListener('loadeddata', () => resolve(), false);
+          webcamElement.addEventListener("loadeddata", () => resolve(), false);
         },
-        error => reject());
+        error => reject()
+      );
     } else {
       reject();
     }
-  })
-};
+  });
+}
 
 let net;
 
-
 async function app() {
-  console.log('Loading mobilenet..');
+  console.log("Loading mobilenet..");
 
   // Load the model.
   net = await mobilenet.load();
-  console.log('Sucessfully loaded model');
+  console.log("Sucessfully loaded model");
 
   await setupWebcam();
+
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, "conv_preds");
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+
+  // When clicking a button, add an example for that class.
+  document
+    .getElementById("class-a")
+    .addEventListener("click", () => addExample(0));
+  document
+    .getElementById("class-b")
+    .addEventListener("click", () => addExample(1));
+  document
+    .getElementById("class-c")
+    .addEventListener("click", () => addExample(2));
+
   while (true) {
     const result = await net.classify(webcamElement);
 
-    document.getElementById('console').innerText = `
+    document.getElementById("console").innerText = `
       style:
       prediction: ${result[0].className}\n
       probability: ${result[0].probability}
@@ -46,7 +72,7 @@ async function app() {
     // fire.
     await tf.nextFrame();
   }
-};
+}
 
 app();
 
@@ -55,6 +81,4 @@ app();
 // let my_floatingBox = document.getElementsByTagName('video')
 
 // floatingBox.append
-
-
-
+// eli
